2e6d275e347144c77013df3775a2ca7accefc9cc
Shawn Perron <sperron3@my.centennialcollege.ca>
2023-03-01 16:23:05 -0500

    Initial commit

---
+++ server/config/config.js
@@ -0,0 +1,3 @@
+//export const MongoURI = "mongodb://localhost:27017/media";
+export const MongoURI = "mongodb+srv://sperron:sperron3@cluster0.dxi3fuc.mongodb.net/?retryWrites=true&w=majority";
+export const Secret = "someSecret";
